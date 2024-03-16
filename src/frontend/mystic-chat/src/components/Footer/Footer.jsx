53ed5a72f384caf5233ab54f0b4f7cdf61c50a76
paulh <paulcjh@protonmail.com>
2024-03-16 21:23:46 +0000

    footer update + input text font size increase

--- src/frontend/mystic-chat/src/components/Footer/Footer.jsx
+++ src/frontend/mystic-chat/src/components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import styles from "./Footer.module.css";
 function Footer(props) {
   return (
     <div className={styles["footer"]}>
-      <div className={styles["footer-row"]}>
+      {/* <div className={styles["footer-row"]}>
         <div className={styles["footer-fill"]} />
         {(props.displayToggle === undefined ||
           props.displayToggle === true) && <DisplayToggle />}
-      </div>
-      <p>Made by <a href="https://www.mystic.ai/" target="_blank" referrerPolicy="origin">Mystic AI</a>. &#129497;</p>
+      </div> */}
+      <div className="text-small text-secondary flex-row flex-right">Made by<a href="https://www.mystic.ai/" target="_blank" referrerPolicy="origin">Mystic AI</a> &#129497;</div>
     </div>
   );
 }
