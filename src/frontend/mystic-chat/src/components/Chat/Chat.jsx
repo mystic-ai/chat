1c699430b0a07b387801993c5b18fde24dbd6938
paulh <paulcjh@protonmail.com>
2024-03-18 13:04:06 +0000

    work

--- src/frontend/mystic-chat/src/components/Chat/Chat.jsx
+++ src/frontend/mystic-chat/src/components/Chat/Chat.jsx
@@ -186,6 +186,17 @@ export default function Chat() {
             <div className={styles["chat-column"]}>
                 <div className={styles["chat-container"]} ref={messageContainerRef}>
                     <div className={styles["chat-header"]}>
+                        {conversation.length > 0 &&
+                            <div className={styles["chat-header-reset-button"]} onClick={() => {
+                                setConversation([]);
+                                setSources([]);
+                            }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M14 15L10 19L14 23" stroke-width="2" />
+                                    <path d="M5.93782 15.5C5.16735 14.1655 4.85875 12.6141 5.05989 11.0863C5.26102 9.55856 5.96064 8.13986 7.05025 7.05025C8.13986 5.96064 9.55856 5.26102 11.0863 5.05989C12.6141 4.85875 14.1655 5.16735 15.5 5.93782C16.8345 6.70829 17.8775 7.89757 18.4672 9.32122C19.0568 10.7449 19.1603 12.3233 18.7615 13.8117C18.3627 15.3002 17.4838 16.6154 16.2613 17.5535C15.0388 18.4915 13.5409 19 12 19" strokeWidth="2" strokeLinecap="round" />
+                                </svg>
+
+                            </div>}
                         CONVERSATION
                     </div>
                     <div
